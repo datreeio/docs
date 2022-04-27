54ce6d36d53d00e5b0417e931fdb81e6a9868967
hadar-co <hadar@datree.io>
2022-04-27 13:34:36 +0300

    fix image quality

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,22 @@ const config = {
   organizationName: 'Datree.io', // Usually your GitHub org/user name.
   projectName: 'Datree docs', // Usually your repo name.
 
-  plugins: ['docusaurus-plugin-sass'],
+  plugins: [
+    [
+      'docusaurus-plugin-sass', {
+      },
+    ],
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
