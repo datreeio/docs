1483e9699b7a712d9e9ea336de587fee3dac12a4
shmuelsa <shmuel@datree.io>
2022-04-27 12:59:16 +0300

    fix: fix custom tab hash url

--- src/components/built-in-rules/CustomTabs.js
+++ src/components/built-in-rules/CustomTabs.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -12,9 +12,9 @@ import { useColorMode } from "@docusaurus/theme-common";
 
 const handleTabChange = () => {
   const path = location.hash.split("#")[1];
-  const selectedTab = tabsItems.findIndex((item) => item.value === path);
-  if (selectedTab === -1) return tabsItems[0].value;
-  return tabsItems[selectedTab].value;
+  const findSelectedTab = tabsItems.findIndex((item) => item.value === path);
+  if (findSelectedTab === -1) return tabsItems[0].value;
+  return tabsItems[findSelectedTab].value;
 };
 
 const CustomTabs = () => {
@@ -22,15 +22,21 @@ const CustomTabs = () => {
   const history = useHistory();
   const [selectedTab, setSelectedTab] = useState(() => handleTabChange());
 
-  const handleSelectTab = (event, value) => {
-    setSelectedTab(value);
+  useEffect(() => {
+    if (!selectedTab) return;
     history.push(`#${selectedTab}`);
+  }, [selectedTab]);
+
+  const handleSelectTab = (event, value) => {
+    const findSelectedTab = tabsItems.findIndex((item) => item.value === value);
+    if (findSelectedTab === -1) setSelectedTab(tabsItems[0].value);
+    setSelectedTab(tabsItems[findSelectedTab].value);
   };
 
   return (
     <>
       <StyledTabs
-        colorMode={colorMode}
+        colormode={colorMode}
         value={selectedTab}
         onChange={handleSelectTab}
       >
@@ -45,13 +51,13 @@ const CustomTabs = () => {
               <ListItem key={item.id}>
                 <ListItemText
                   primary={
-                    <StyledLink colorMode={colorMode} href={item.slug}>
+                    <StyledLink colormode={colorMode} href={item.slug}>
                       {item.title}
                     </StyledLink>
                   }
                   secondary={
-                    <StyledSubText colorMode={colorMode}>
-                      Enabled by default?{" "}
+                    <StyledSubText colormode={colorMode}>
+                      Enabled by default?
                       {item.isEnabledByDefault ? "True" : "False"}
                     </StyledSubText>
                   }
@@ -67,44 +73,44 @@ const CustomTabs = () => {
 const StyledTabs = styled(Tabs)`
   .MuiTabs-indicator {
     background-color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
   .MuiTab-textColorPrimary.Mui-selected {
     color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
   .MuiTab-wrapper {
     color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
   .MuiTab-wrapper:hover {
     color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
   .MuiTab-textColorPrimary {
     color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
 `;
 
 const StyledSubText = styled.span`
-  color: ${(props) => (props.colorMode === "dark" ? "#fff" : "#000")};
+  color: ${(props) => (props.colormode === "dark" ? "#fff" : "#000")};
 `;
 
 const StyledLink = styled(Link)`
   color: ${(props) =>
-    props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+    props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   .MuiTab-wrapper {
     color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
   .MuiTab-wrapper:hover {
     color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
   .MuiTab-textColorPrimary {
     color: ${(props) =>
-      props.colorMode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
+      props.colormode === "dark" ? "var(--ifm-menu-color-active)" : "#000"};
   }
 `;
 
