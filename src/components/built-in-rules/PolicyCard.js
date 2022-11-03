a2fcd755b8455be45b5a5ed20ce49176b2f6fd35
hadar-co <hadar@datree.io>
2022-11-03 10:25:21 +0200

    add policy cards

--- src/components/built-in-rules/PolicyCard.js
+++ src/components/built-in-rules/PolicyCard.js
@@ -11,26 +11,27 @@ import { borderColor } from "@mui/system";
 
 class PolicyCard extends Component {
   state= {
-    border: 0,
+    border: 0.5,
+    borderColor: "grey"
   }
 
-  onMouseOver = () => this.setState({ border: 1 });
+  onMouseOver = () => this.setState({ border: 0.5, borderColor: "cornflowerblue" });
 
-  onMouseOut = () => this.setState({ border: 0 });
+  onMouseOut = () => this.setState({ border: 0.5, borderColor: "grey" });
 
-render(props) {
-  return (
-    <Link href={this.props.link} style={{ textDecoration: 'none' }} >
-      <Card sx={{ maxWidth: 360, border: this.state.border, boxShadow: 5, borderBox: 8, borderColor: "cornflowerblue" }} onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut} >
-        <Box sx={{ my: 1 }}>
-          <CardMedia component="img" image={this.props.image} height="80" sx={{ objectFit: "contain" }} style={{ margin: "1rem 0" }} />
-        </Box>
-        <Divider variant="middle" light />  
-        <CardHeader style={{ margin: "1rem", padding: 0 }} title={this.props.title} align="center" subheader={this.props.subtitle} titleTypographyProps={{ fontWeight: "medium" }} />
-          
-      </Card>
-    </Link>
-  );
-};
+  render(props) {
+    return (
+      <Link href={this.props.link} style={{ textDecoration: 'none' }} >
+        <Card sx={{ maxWidth: 360, border: this.state.border, borderStyle: 2, boxShadow: 5, borderRadius: 4, borderColor: this.state.borderColor }} onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut} >
+          <Box sx={{ my: 1 }}>
+            <CardMedia component="img" image={this.props.image} height="80" sx={{ objectFit: "contain" }} style={{ margin: "1rem 0" }} />
+          </Box>
+          <Divider variant="middle" light />  
+          <CardHeader style={{ margin: "1rem", padding: 0 }} title={this.props.title} align="center" subheader={this.props.subtitle} titleTypographyProps={{ fontWeight: "medium" }} />
+            
+        </Card>
+      </Link>
+    );
+  };
 };
 export default PolicyCard;
