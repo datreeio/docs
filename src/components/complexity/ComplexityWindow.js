a1648029705716b8f508f5c9cccf92ecaaed4f0d
hadar-co <hadar@datree.io>
2023-01-03 14:06:04 +0200

    add images to policies (#125)
    * add images to policies
    
    * add images to policies
    
    * add images to policies
    
    * add images to policies

--- src/components/complexity/ComplexityWindow.js
+++ src/components/complexity/ComplexityWindow.js
@@ -18,33 +18,10 @@ const Complexity = () => (
           {' '}
           A rule's complexity describes how simple/difficult it is to fix the relevant misconfiguration:<br/><br/>
           <b>Easy - </b>set or refrain from setting a boolean key/value.<br/>
-          <b>Medium - </b>set or refrain from setting a key/value from a pre-defined list of possible options. May require simple human intervention<br/>
+          <b>Medium - </b>set or refrain from setting a key/value from a pre-defined list of possible options. May require simple human intervention.<br/>
           <b>Hard - </b>the fix requires research, changes in other resources, and/or more intricate human intervention.<br/>
           
         </div>
-        {/* <div className="actions">
-          <Popup
-            trigger={<button className="button"> Trigger </button>}
-            position="top center"
-            nested
-          >
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-              magni omnis delectus nemo, maxime molestiae dolorem numquam
-              mollitia, voluptate ea, accusamus excepturi deleniti ratione
-              sapiente! Laudantium, aperiam doloribus. Odit, aut.
-            </span>
-          </Popup>
-          <button
-            className="button"
-            onClick={() => {
-              console.log('modal closed ');
-              close();
-            }}
-          >
-            close modal
-          </button>
-        </div> */}
       </div>
     )}
   </Popup>
